65c525920d57ec9f04f2539feab8cbc6a145ad3f
Wuxd <wuxd711722@126.com>
2016-03-11 09:58:18 +0800

    modified

--- X_API_Master/X.Util.Core/ExtensionMethods.cs
+++ X_API_Master/X.Util.Core/ExtensionMethods.cs
@@ -8,6 +8,7 @@ namespace X.Util.Core
 {
     public static class ExtensionMethods
     {
+        #region 序列化
         private static readonly BinaryFormatter BinaryFormatter = new BinaryFormatter();
         public static bool IsJson(this string json)
         {
@@ -47,13 +48,10 @@ namespace X.Util.Core
         public static T Deserialize<T>(this byte[] b)
         {
             return (T)BinaryFormatter.Deserialize(new MemoryStream(b));
-        }
+        } 
+        #endregion
 
-        public static bool RegexContains(this string input, string item, string split)
-        {
-            return Regex.IsMatch(input, "(^|" + split + ")" + item + "(" + split + "|$)");
-        }
-
+        #region decimal
         public static string FloorString(this decimal d, int length)
         {
             return (Math.Floor(d * (long)Math.Pow(10, length)) / (long)Math.Pow(10, length)).ToString("N" + length);
@@ -72,11 +70,43 @@ namespace X.Util.Core
         public static decimal CeilingDecimal(this decimal d, int length)
         {
             return Math.Ceiling(d * (long)Math.Pow(10, length)) / (long)Math.Pow(10, length);
+        } 
+        #endregion
+
+        #region string
+        public static bool IsNullOrEmpty(this string s)
+        {
+            return string.IsNullOrEmpty(s);
         }
 
+        public static bool IsNullOrWhiteSpace(this string s)
+        {
+            return string.IsNullOrWhiteSpace(s);
+        }
+
+        public static bool RegexContains(this string input, string item, string split)
+        {
+            return Regex.IsMatch(input, "(^|" + split + ")" + item + "(" + split + "|$)");
+        } 
+        #endregion
+
+        #region object
+        public static bool IsNull(this object o)
+        {
+            return o == null;
+        }
+        #endregion
+
+        #region Class
         public static Version Add(this Version v, int major, int minor, int build, int revision)
         {
             return new Version(Math.Max(v.Major, 0) + major, Math.Max(v.Minor, 0) + minor, Math.Max(v.Build, 0) + build, Math.Max(v.Revision, 0) + revision);
         }
+
+        public static T EnsureNotNull<T>(this T t) where T : class
+        {
+            return t ?? Activator.CreateInstance<T>();
+        }
+        #endregion
     }
 }
