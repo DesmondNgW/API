a23672ac70df694296cbb2f2521ddcef90b29efe
Wuxd <wuxd711722@126.com>
2016-05-05 11:37:42 +0800

    modified

---
+++ X_API_Master/X.Util.Entities/Interface/ICouchCache.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace X.Util.Entities.Interface
+{
+    public interface ICouchCache
+    {
+        object Get(string key);
+
+        string GetJson(string key);
+
+        object Get(string key, DateTime dt);
+
+        string GetJson(string key, DateTime dt);
+
+        T Get<T>(string key);
+
+        T GetJson<T>(string key);
+
+        T Get<T>(string key, DateTime dt);
+
+        T GetJson<T>(string key, DateTime dt);
+
+        bool TryGet<T>(string key, out T obj);
+
+        bool TryGetJson<T>(string key, out T obj);
+
+        bool TryGet<T>(string key, DateTime dt, out T obj);
+
+        bool TryGetJson<T>(string key, DateTime dt, out T obj);
+
+        void Set(string key, object obj);
+
+        void SetJson(string key, object obj);
+
+        void Set(string key, object obj, DateTime dt);
+
+        void SetJson(string key, object obj, DateTime dt);
+
+        void Set(string key, object obj, TimeSpan ts);
+
+        void SetJson(string key, object obj, TimeSpan ts);
+
+        void Remove(string key);
+    }
+}
