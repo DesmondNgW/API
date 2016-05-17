e74d38859083cea727997bf74504211aedcfc59b
Wuxd <wuxd711722@126.com>
2016-05-17 16:11:55 +0800

    modified

--- X_API_Master/X.Util.Entities/Event.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace X.Util.Entities
-{
-    public class Event<T>
-    {
-        public int HashCode { get; set; }
-        public string Name { get; set; }
-        public T Target { get; set; }
-    }
-}
