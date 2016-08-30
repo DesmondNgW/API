2358a9f6468d4436817e3a73868da9fd4f0f5ec0
Wuxd <wuxd711722@126.com>
2016-08-30 14:39:11 +0800

    modified

--- X_API_Master/X.Interface.Core/ServiceHelper.cs
+++ X_API_Master/X.Interface.Core/ServiceHelper.cs
@@ -41,24 +41,67 @@ namespace X.Interface.Core
         public string Token { get; set; }
 
         /// <summary>
-        /// StatusZone
+        /// 用户状态存储的分区
         /// </summary>
         public int StatusZone { get; set; }
     }
 
+    /// <summary>
+    /// 分区号
+    /// </summary>
     public enum Zone
     {
-        TradeZero = 0,
-        TradeOne = 1,
-        TradeTwo = 2,
-        TradeThree = 3,
-        TradeFour = 4,
-        TradeFive = 5,
-        TradeSix = 6,
-        TradeSeven = 7,
-        TradeEight = 8,
-        TradeNine = 9,
-        TradeTen = 10,
+        Trade0 = 0,
+        Trade1 = 1,
+        Trade2 = 2,
+        Trade3 = 3,
+        Trade4 = 4,
+        Trade5 = 5,
+        Trade6 = 6,
+        Trade7 = 7,
+        Trade8 = 8,
+        Trade9 = 9,
+        Trade10 = 10,
+        Trade11 = 11,
+        Trade12 = 12,
+        Trade13 = 13,
+        Trade14 = 14,
+        Trade15 = 15,
+        Trade16 = 16,
+        Trade17 = 17,
+        Trade18 = 18,
+        Trade19 = 19,
+        Trade110 = 20,
+        Trade21 = 21,
+        Trade22 = 22,
+        Trade23 = 23,
+        Trade24 = 24,
+        Trade25 = 25,
+        Trade26 = 26,
+        Trade27 = 27,
+        Trade28 = 28,
+        Trade29 = 29,
+        Trade30 = 30,
+        Trade31 = 31,
+        Trade32 = 32,
+        Trade33 = 33,
+        Trade34 = 34,
+        Trade35 = 35,
+        Trade36 = 36,
+        Trade37 = 37,
+        Trade38 = 38,
+        Trade39 = 39,
+        Trade40 = 40,
+        Trade41 = 41,
+        Trade42 = 42,
+        Trade43 = 43,
+        Trade44 = 44,
+        Trade45 = 45,
+        Trade46 = 46,
+        Trade47 = 47,
+        Trade48 = 48,
+        Trade49 = 49,
+        Trade50 = 50
     }
 
     /// <summary>
