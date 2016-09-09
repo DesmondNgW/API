03a0edce8519175907f8ed28d1cba5eb53f89fed
Wuxd <wuxd711722@126.com>
2016-09-09 14:16:28 +0800

    modified

---
+++ X_API_Master/X.Stock.Monitor/StockService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace X.Stock.Monitor
+{
+    public partial class StockService : ServiceBase
+    {
+        public StockService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ X_API_Master/X.Stock.Monitor/uninstall.bat
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe -u X.Win.TaskService.exe
+pause
