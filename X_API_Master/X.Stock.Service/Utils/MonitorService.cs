81e5e8657ee14e47b90dbe15660c46e361902401
Wuxd <wuxd711722@126.com>
2016-09-19 14:15:01 +0800

    modified

--- X_API_Master/X.Stock.Service/Utils/MonitorService.cs
+++ X_API_Master/X.Stock.Service/Utils/MonitorService.cs
@@ -14,7 +14,7 @@ namespace X.Stock.Service.Utils
         {
             var targets = stocks.Where(p => p.StockKm2 >= 3.3M && p.StockKm2 <= 8.8M);
             var list = GetStockMonitor();
-            foreach (var target in targets.Where(target => list.Count(p => p.Id == target.StockCode + "_" + DateTime.Now.ToString("yyyyMMdd")) == 0))
+            foreach (var target in targets.Where(target => list.Count(p => p.Id == target.StockCode + "_" + DateTime.Now.ToString("yyyyMMdd") + "_Buy") == 0))
             {
                 var msg = string.Format("Stock Code {0}({1}) has at price {2},inc {3}%", target.StockCode, target.StockName, target.StockPrice, target.StockKm2);
                 SmtpMailHelper.Send("Stock." + target.StockCode, msg);
@@ -35,7 +35,7 @@ namespace X.Stock.Service.Utils
         {
             var targets = stocks.Where(p => p.StockKm2 <=-3.3M);
             var list = GetStockMonitor();
-            foreach (var target in targets.Where(target => list.Count(p => p.Id == target.StockCode + "_" + DateTime.Now.ToString("yyyyMMdd")) == 0))
+            foreach (var target in targets.Where(target => list.Count(p => p.Id == target.StockCode + "_" + DateTime.Now.ToString("yyyyMMdd") + "_Sell") == 0))
             {
                 var msg = string.Format("Stock Code {0}({1}) has at price {2},inc {3}%", target.StockCode, target.StockName, target.StockPrice, target.StockKm2);
                 SmtpMailHelper.Send("Stock." + target.StockCode, msg);
