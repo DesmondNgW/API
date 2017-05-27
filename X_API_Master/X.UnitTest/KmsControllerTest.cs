f60a1a6811dc9bf81dc0c843df46ca82a659c476
Wuxd <wuxd711722@126.com>
2017-05-27 13:49:32 +0800

    uuu

---
+++ X_API_Master/X.UnitTest/KmsControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using X.UI.API.Controllers;
+
+namespace X.UnitTest
+{
+    [TestClass]
+    public class KmsControllerTest
+    {
+        [TestMethod]
+        public void Now()
+        {
+            var api = new KmsController();
+            var ret = api.Now();
+            Assert.AreEqual(ret.Data, DateTime.Now);
+        }
+    }
+}
