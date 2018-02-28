24145a1a71a3239ca365d13b44e94b785f0c8642
Wuxd <wuxd711722@126.com>
2018-02-28 17:54:08 +0800

    uu

--- X_API_Master/X.UI.Consoles/Program.cs
+++ X_API_Master/X.UI.Consoles/Program.cs
@@ -15,88 +15,6 @@ using X.Util.Other;
 
 namespace X.UI.Consoles
 {
-    //public class CaiLianNews
-    //{
-    //    public decimal error { get; set; }
-
-    //    public Tmp data { get; set; }
-    //}
-
-    //public class Tmp
-    //{
-    //    public List<RollData> roll_data { get; set; }
-    //}
-
-    //public class RollData
-    //{
-    //    public int is_top { get; set; }
-
-    //    public long modified_time { get; set; }
-
-    //    public string share_img { get; set; }
-
-    //    public string img { get; set; }
-
-    //    public int jpush { get; set; }
-
-    //    public string level { get; set; }
-
-    //    public string author_extends { get; set; }
-
-    //    public int bold { get; set; }
-
-    //    public int comment_num { get; set; }
-
-    //    public int deny_comment { get; set; }
-
-    //    public int user_id { get; set; }
-
-    //    //public string[] imgs { get; set; }
-
-    //    public string shareurl { get; set; }
-
-    //    public long ctime { get; set; }
-
-    //    public long sort_score { get; set; }
-
-    //    public int status { get; set; }
-
-    //    public int reading_num { get; set; }
-
-    //    public int in_roll { get; set; }
-
-    //    public string title { get; set; }
-
-    //    public string[] images { get; set; }
-
-    //    public string[] tags { get; set; }
-
-    //    public string content { get; set; }
-
-    //    public string brief { get; set; }
-
-    //    public int type { get; set; }
-
-    //    //public string[] sub_titles { get; set; }
-
-    //    public int id { get; set; }
-
-    //    public int recommend { get; set; }
-
-    //    public string depth_extends { get; set; }
-
-    //    public int explain_num { get; set; }
-
-    //    public int has_img { get; set; }
-
-    //    public string category { get; set; }
-
-    //    public int collection { get; set; }
-
-    //    public int confirmed { get; set; }
-    //}
-
-
     class Program
     {
         static void Main()
@@ -113,9 +31,7 @@ namespace X.UI.Consoles
             //var s = MongoDbBase<MongoTest>.Default.FindBsonDocument("Test", "test", Query.Null);
             //var f = MongoDbBase<MongoTest>.ToEntity(s);
 
-            //var urlFormat = "https://www.cailianpress.com/nodeapi/telegraphs?last_time={0}&refresh_type=1&rn=20";
-            //var ret = ApiData.Get<CaiLianNews>(urlFormat, null, null);
-            //Console.ReadKey();
+            Console.ReadKey();
         }
 
         static void Index()
