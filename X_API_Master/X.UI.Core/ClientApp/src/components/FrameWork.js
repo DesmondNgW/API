c70ce97d4dd86bd26e8ec5e91bea3ed445197feb
wuxd7 <wuxd711722@126.com>
2018-11-25 15:05:43 +0800

    rt

---
+++ X_API_Master/X.UI.Core/ClientApp/src/components/FrameWork.js
@@ -0,0 +1,12 @@
+﻿import React, { Component } from 'react';
+
+export class FrameWork extends Component {
+    displayName = FrameWork.name
+
+    render() {
+        if (props.box) {
+            return this.props.children
+        }
+        return <div className="__framework-box">{this.props.children}</div>
+    }
+}
