74530d33bc7021331811ffefabca8fa817ff66e0
desmondNg <wuxd711722@126.com>
2022-05-01 10:19:50 +0800

    7

---
+++ X_API_Master/X.Interface.Dto/Interface/IStockManager.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace X.Interface.Dto.Interface
+{
+    /// <summary>
+    /// 股票接口
+    /// </summary>
+    public interface IStockManager
+    {
+        /// <summary>
+        /// 复盘
+        /// </summary>
+        /// <returns></returns>
+        ApiResult<bool> Replay();
+
+        /// <summary>
+        /// 选股
+        /// </summary>
+        /// <param name="TopCount"></param>
+        /// <returns></returns>
+        ApiResult<bool> SelectStock(int TopCount);
+
+        /// <summary>
+        /// 保存题材数据 
+        /// </summary>
+        /// <returns></returns>
+        ApiResult<bool> SaveDataBase();
+    }
+}
