682f9f313b732b26a6e668632a28b742b90b3712
desmondNg <wuxd711722@126.com>
2022-05-01 10:18:31 +0800

    版本

---
+++ X_API_Master/X.Interface.Core/Helper/LoginServiceHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using X.Business.Helper;
+
+namespace X.Interface.Core.Helper
+{
+    /// <summary>
+    /// 登录状态
+    /// </summary>
+    public struct LoginStatus
+    {
+        /// <summary>
+        /// 用户编号
+        /// </summary>
+        public string CustomerNo { get; set; }
+
+        /// <summary>
+        /// 用户所属分区
+        /// </summary>
+        public int Zone { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string CustomerName { get; set; }
+    }
+
+
+    public class LoginServiceHelper
+    {
+        /// <summary>
+        /// 验证token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="clientId"></param>
+        /// <param name="userAgent"></param>
+        /// <param name="uri"></param>
+        /// <param name="clientIp"></param>
+        public static void VerifyToken(string token, string clientId, string clientIp, string userAgent, Uri uri)
+        {
+            TokenHelper.VerifyToken(token, clientId, clientIp, userAgent, uri);
+        }
+
+        /// <summary>
+        /// 设置登录状态
+        /// </summary>
+        /// <param name="customerNo"></param>
+        /// <param name="customerName"></param>
+        /// <param name="zone"></param>
+        /// <param name="clientId"></param>
+        /// <param name="ip"></param>
+        /// <param name="userAgent"></param>
+        /// <returns></returns>
+        public static string SetLoginStatus(string customerNo, string customerName, int zone, string clientId, string ip, string userAgent)
+        {
+            return LoginStatusHelper.SetLoginStatus(customerNo, customerName, zone, clientId, ip, userAgent);
+        }
+
+        /// <summary>
+        /// 清除登录状态
+        /// </summary>
+        /// <param name="uid"></param>
+        public static void Clear(string uid)
+        {
+            LoginStatusHelper.Clear(uid);
+        }
+
+        /// <summary>
+        /// 用户验证
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="uid"></param>
+        /// <param name="clientId"></param>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        public static LoginStatus UserIdentity(string token, string uid, string clientId, Uri uri)
+        {
+            var status = LoginStatusHelper.UserIdentity(token, uid, clientId, uri);
+            return new LoginStatus
+            {
+                CustomerName = status.CustomerName,
+                CustomerNo = status.CustomerNo,
+                Zone = status.Zone
+            };
+        }
+    }
+}
---
+++ X_API_Master/X.Interface.Core/Stock/StockManagerService.cs
@@ -0,0 +1,73 @@
+﻿using X.Business.Core.Stock;
+using X.Business.Entities.Enums;
+using X.Business.Helper.Stock;
+using X.Interface.Dto;
+using X.Interface.Dto.Interface;
+
+namespace X.Interface.Core.Stock
+{
+    public class StockManagerService: IStockManager
+    {
+        /// <summary>
+        /// 复盘
+        /// </summary>
+        /// <param name="AQS"></param>
+        /// <param name="Wave"></param>
+        /// <returns></returns>
+        public ApiResult<bool> Replay()
+        {
+            var AQS = StockDealIO.GetMyStock(MyStockMode.AQS);
+            var Wave = StockDealIO.GetMyStock(MyStockMode.Wave);
+            StockDealIO.Deal(AQS, Wave);
+            return new ApiResult<bool>
+            {
+                Data = true,
+                Success = true
+            };
+        }
+
+        /// <summary>
+        /// 选股
+        /// </summary>
+        /// <param name="TopCount"></param>
+        /// <returns></returns>
+        public ApiResult<bool> SelectStock(int TopCount)
+        {
+            var first = StockDealIO.GetMyMonitorStock(MyStockType.First);
+            var zt = StockDealIO.GetMyMonitorStock(MyStockType.ZT);
+            var kernel = StockDealIO.GetMyMonitorStock(MyStockType.Kernel);
+            var kernelH = StockDealIO.GetMyMonitorStock(MyStockType.KernelH);
+            var kernelL = StockDealIO.GetMyMonitorStock(MyStockType.KernelL);
+            var coreT = StockDealIO.GetMyMonitorStock(MyStockType.CoreT);
+            var coreT2 = StockDealIO.GetMyMonitorStock(MyStockType.CoreT2);
+            var coreT3 = StockDealIO.GetMyMonitorStock(MyStockType.CoreT3);
+            var ldx = StockDealIO.GetMyMonitorStock(MyStockType.LDX);
+            var ddx = StockDealIO.GetDDXList();
+            var AQS = StockDealIO.GetMyStock(MyStockMode.AQS);
+            var Wave = StockDealIO.GetMyStock(MyStockMode.Wave);
+            var all = StockDealBase.Union(AQS, Wave);
+            StockDealIO.FilterStock(first, zt, kernel, kernelH, kernelL, coreT, coreT2, coreT3, ldx, ddx, AQS, all, TopCount);
+            return new ApiResult<bool>
+            {
+                Data = true,
+                Success = true
+            };
+        }
+
+        /// <summary>
+        /// 保存题材数据 
+        /// </summary>
+        /// <param name="Bak"></param>
+        /// <returns></returns>
+        public ApiResult<bool> SaveDataBase()
+        {
+            var bak = StockDealIO.GetStockDes(StockDesType.Bak);
+            StockDealIO.SaveDataBase(bak);
+            return new ApiResult<bool>
+            {
+                Data = true,
+                Success = true
+            };
+        }
+    }
+}
