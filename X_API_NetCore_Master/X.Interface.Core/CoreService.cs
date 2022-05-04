a552f4f32504653df3d099952f6d7f691c48558d
desmondNg <wuxd711722@126.com>
2022-05-04 21:14:40 +0800

    67

---
+++ X_API_NetCore_Master/X.Interface.Core/CoreService.cs
@@ -0,0 +1,12 @@
+﻿using X.Interface.Dto;
+
+namespace X.Interface.Core
+{
+    public class CoreService
+    {
+        public static bool CallSuccess<TResult>(ApiResult<TResult> iresult)
+        {
+            return iresult != null && iresult.Data != null && iresult.Success;
+        }
+    }
+}
--- X_API_NetCore_Master/X.UI.WebAPI/Controllers/RequestManagerController.cs
+++ X_API_NetCore_Master/X.UI.WebAPI/Controllers/RequestManagerController.cs
@@ -2,7 +2,8 @@
 using X.Interface.Core;
 using X.Interface.Dto;
 using X.Interface.Dto.HttpResponse;
-using X.Interface.Dto.Interface;
+using X.Util.Core.Kernel;
+using X.Util.Entities;
 using X.Util.Provider;
 
 namespace X.UI.WebAPI.Controllers
@@ -14,24 +15,25 @@ namespace X.UI.WebAPI.Controllers
         [HttpGet(Name = "GetTimestamp")]
         public ApiResult<string> GetTimestamp()
         {
-            var provider = new InstanceProvider<RequestManagerService>();
-            return provider.Client.GetTimestamp();
+            var provider = new InstanceProvider<RequestManagerService>(LogDomain.Ui);
+            return CoreAccess<RequestManagerService>.TryCall<ApiResult<string>>(provider.Client.GetTimestamp,
+                provider, new LogOptions<ApiResult<string>>(CoreService.CallSuccess));
         }
 
         [HttpGet(Name = "GetToken")]
         public ApiResult<string> GetToken(string clientId, string ip)
         {
-            var provider = new InstanceProvider<RequestManagerService>();
-            return provider.Client.GetToken(clientId, ip);
+            var provider = new InstanceProvider<RequestManagerService>(LogDomain.Ui);
+            return CoreAccess<RequestManagerService>.TryCall<ApiResult<string>, string, string>(provider.Client.GetToken, 
+                clientId, ip,provider, new LogOptions<ApiResult<string>>(CoreService.CallSuccess));
         }
 
         [HttpGet(Name = "GetPublicKey")]
         public ApiResult<PublicKeyDto> GetPublicKey(int size)
         {
-            var provider = new InstanceProvider<KeyManagerService>();
-            return provider.Client.GetPublicKey(size);
+            var provider = new InstanceProvider<KeyManagerService>(LogDomain.Ui);
+            return CoreAccess<KeyManagerService>.TryCall<ApiResult<PublicKeyDto>, int>(provider.Client.GetPublicKey, size,
+                provider, new LogOptions<ApiResult<PublicKeyDto>>(CoreService.CallSuccess));
         }
-
-
     }
 }
