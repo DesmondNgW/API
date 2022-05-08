1a10bb754a4fdfe48924e9dc638b9e3f9a67fae2
desmondNg <wuxd711722@126.com>
2022-05-08 20:44:35 +0800

    67

--- X_API_NetCore_Master/X.UI.Util/Controller/ControllerBaseWithOutToken.cs
+++ X_API_NetCore_Master/X.UI.Util/Controller/ControllerBaseWithOutToken.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc;
 using X.UI.Util.Filter;
 
 namespace X.UI.Util.Controller
--- X_API_NetCore_Master/X.UI.WebAPI/Controllers/ImageManagerController.cs
+++ X_API_NetCore_Master/X.UI.WebAPI/Controllers/ImageManagerController.cs
@@ -1,13 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using X.Interface.Core;
-using X.Interface.Dto;
-using X.Interface.Dto.HttpResponse;
-using X.Interface.Dto.Interface;
 using X.UI.Util.Controller;
-using X.Util.Core.Kernel;
-using X.Util.Entities;
-using X.Util.Extend.Core;
-using X.Util.Provider;
+using X.Util.Other;
 
 namespace X.UI.WebAPI.Controllers
 {
@@ -16,27 +9,35 @@ namespace X.UI.WebAPI.Controllers
     public class ImageManagerController : ControllerBaseWithOutFilter
     {
         [HttpGet(Name = "VerifyCode")]
-        public HttpResponseMessage VerifyCode()
+        public void VerifyCode()
         {
-            var provider = new InstanceProvider<ImageManagerService>(LogDomain.Ui);
-            return CoreAccess<IImageManager>.TryCall<HttpResponseMessage>(provider.Client.VerifyCode, provider, 
-                new LogOptions<HttpResponseMessage>(CoreBase.CallSuccess));
+            var vc = CaptchaHelper.CaptchaOptions();
+            var result = CaptchaHelper.GetCaptchaByte(vc);
+            Response.ContentType = "image/png";
+            Response.StatusCode = 200;
+            Response.Headers.Add("vc", CaptchaHelper.GetCaptchaCode("VerifyCode", vc.Value));
+            Response.Body.WriteAsync(result, 0, result.Length);
         }
 
         [HttpGet(Name = "BitMap")]
-        public HttpResponseMessage BitMap()
+        public void BitMap()
         {
-            var provider = new InstanceProvider<ImageManagerService>(LogDomain.Ui);
-            return CoreAccess<IImageManager>.TryCall<HttpResponseMessage>(provider.Client.BitMap, provider,
-                new LogOptions<HttpResponseMessage>(CoreBase.CallSuccess));
+            var result = CaptchaHelper.GetBitMapByte();
+            Response.ContentType = "image/png";
+            Response.StatusCode = 200;
+            Response.Body.WriteAsync(result, 0, result.Length);
         }
 
         [HttpGet(Name = "TextImage")]
-        public HttpResponseMessage TextImage(string value)
+        public void TextImage(string value)
         {
-            var provider = new InstanceProvider<ImageManagerService>(LogDomain.Ui);
-            return CoreAccess<IImageManager>.TryCall<HttpResponseMessage, string>(provider.Client.TextImage, value, provider,
-                new LogOptions<HttpResponseMessage>(CoreBase.CallSuccess));
+            var opt = CaptchaHelper.TextImageOptions();
+            opt.Value = value;
+            var result = CaptchaHelper.GetTextImageByte(opt);
+            Response.ContentType = "image/png";
+            Response.StatusCode = 200;
+            Response.Headers.Add("opt", CaptchaHelper.GetCaptchaCode("TextImage", opt.Value));
+            Response.Body.WriteAsync(result, 0, result.Length);
         }
     }
 }
