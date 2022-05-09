ca8bac2581b337869eff08c4eea1b16112a5c087
desmondNg <wuxd711722@126.com>
2022-05-09 10:54:24 +0800

    7

---
+++ X_API_NetCore_Master/X.Interface.Dto/HttpResponse/RequestPreDto.cs
@@ -0,0 +1,11 @@
+﻿namespace X.Interface.Dto.HttpResponse
+{
+    public class RequestPreDto
+    {
+        public string Token { get; set; }
+
+        public long Timestamp { get; set; }
+
+        public string Version { get; set; }
+    }
+}
