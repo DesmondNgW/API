43d1acaf207456c37f357ca1d8f942e77ff5a53c
desmondNg <wuxd711722@126.com>
2022-05-11 21:31:37 +0800

    678

--- X_API_NetCore_Master/X.UI.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace X.UI.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
