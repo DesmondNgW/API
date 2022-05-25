79fb479a95f252697c0f987bf16253b34fd9c61e
desmondNg <wuxd711722@126.com>
2022-05-25 09:52:25 +0800

    8

---
+++ X_API_NetCore_Master/X.Interface.Dto/HttpRequest/ApiRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace X.Interface.Dto.HttpRequest
+{
+    public class ApiRequestDto
+    {
+        public string Uri { get; set; }
+
+        public object Postdata { get; set; }
+
+        public Dictionary<string, string> ExtendHeaders { get; set; }
+    }
+}
