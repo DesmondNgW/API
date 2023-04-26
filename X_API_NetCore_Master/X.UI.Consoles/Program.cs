27311770064697fc8cdbb899b70d6d3bb90c951e
desmondNg <wuxd711722@126.com>
2023-04-26 20:38:59 +0800

    new project

--- X_API_NetCore_Master/X.UI.Consoles/Program.cs
+++ X_API_NetCore_Master/X.UI.Consoles/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using X.UI.Consoles;
 
-MethodHelper.MonitorTcpConnection();
+//MethodHelper.MonitorTcpConnection();
 Console.ReadLine();
