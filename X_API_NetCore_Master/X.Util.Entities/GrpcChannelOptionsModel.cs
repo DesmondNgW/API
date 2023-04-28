c845d67f3f8ef0e9cf62393e00f99e3df6b1165e
desmondNg <wuxd711722@126.com>
2023-04-28 13:03:02 +0800

    ty

---
+++ X_API_NetCore_Master/X.Util.Entities/GrpcChannelOptionsModel.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Net.Client;
+
+namespace X.Util.Entities
+{
+    public class GrpcChannelOptionsModel
+    {
+        public GrpcChannelOptions GrpcChannelOptions { get; set; }
+
+        public string GrpcAddress { get; set; }
+
+        public string GrpcName { get; set; }
+    }
+}
---
+++ X_API_NetCore_Master/X.Util.Provider/GrpcProxyPoolProvider.cs
@@ -0,0 +1,89 @@
+﻿using Grpc.Net.Client;
+using System;
+using X.Util.Core.Configuration;
+using X.Util.Entities;
+using X.Util.Entities.Interface;
+
+namespace X.Util.Provider
+{
+    /// <summary>
+    /// GrpcProxyPoolProvider
+    /// </summary>
+    /// <typeparam name="TChannel"></typeparam>
+    public sealed class GrpcProxyPoolProvider<TChannel> : IClientProvider<TChannel>, IDisposable
+    {
+        #region 构造函数
+        public readonly LogDomain EDomain = LogDomain.Core;
+        public readonly IProxyPooled<TChannel> ProxyPooledManager;
+        public readonly GrpcChannelOptionsModel GrpcModel = ConfigurationHelper.GetGrpcChannelByName(typeof(TChannel).FullName);
+        private IProxyPooled<TChannel> GetProxyPooledManager()
+        {
+            return new ProxyPooledManager<TChannel>(new ProxyPooledRequest<TChannel>
+            {
+                PoolSize = 20,
+                EndpointAddress = EndpointAddress,
+                CreateChannel = CreateChannel,
+                InitChannel = InitChannel,
+                ChannelLifeCycle = new TimeSpan(0, 0, 0),
+                ChannelRecycle = new TimeSpan(0, 1, 0),
+            });
+        }
+
+        public GrpcProxyPoolProvider()
+        {
+            ProxyPooledManager = GetProxyPooledManager();
+        }
+
+        public GrpcProxyPoolProvider(LogDomain eDomain)
+        {
+            EDomain = eDomain;
+            ProxyPooledManager = GetProxyPooledManager();
+        }
+        #endregion
+
+        #region 内部实现
+        private static TChannel _instance;
+        private void InitChannel(TChannel channel, Action<TChannel> releaseClient)
+        {
+        }
+
+        private TChannel CreateChannel()
+        {
+            var channel = GrpcChannel.ForAddress(GrpcModel.GrpcAddress, GrpcModel.GrpcChannelOptions);
+            return (TChannel)Activator.CreateInstance(typeof(TChannel), new object[] { channel });
+        }
+        #endregion
+
+        #region 对外公开方法和属性
+        public string EndpointAddress => GrpcModel.GrpcAddress;
+
+        public LogDomain Domain => EDomain;
+
+        public TChannel Client
+        {
+            get
+            {
+                _instance = ProxyPooledManager.GetClient();
+                return _instance;
+            }
+        }
+
+        public void ReleaseClient()
+        {
+            ProxyPooledManager.ReleaseClient(_instance);
+        }
+
+        public void Dispose(LogDomain eDomain)
+        {
+            ProxyPooledManager.ReleaseClient(_instance);
+        }
+
+        public void Dispose()
+        {
+            Dispose(EDomain);
+        }
+        #endregion
+    }
+
+
+}
